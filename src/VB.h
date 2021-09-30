190f23d7c88c471ba819304f694128b2fc81816a
HeliumHe-He <bartol.hrg.backup@gmail.com>
2021-09-30 19:48:12 +0200

    EXTERNAL, RETURN

--- src/VB.h
+++ src/VB.h
@@ -7,9 +7,11 @@
 #define DO do {
 #define ALLTHEWHILE } while (
 #define ENDDO );
+
 #define WHILE while (
 #define DOTHIS ) {
 #define ENDWHILE }
+
 #define FOR for (
 #define ENDFOR }
 
@@ -18,14 +20,20 @@
 
 #define AS ,
 #define OF ,
+#define OFSIZE ,
 
 #define ___DECLARE(var, type) type var
 #define DECLARE(var_AS_type) ___DECLARE(var_AS_type)
 
+#define PRIVATE static
+
 #define ___CAST(exp, type) ((type) (exp))
 #define CAST(expression_AS_type) ___CAST(expression_AS_type)
 
-#define	___FUNCTION(name, args, type) ___DECLARE(name args, type)
+#define ___ARRAY(name, size) name[size]
+#define ARRAY(name_OF_SIZE_size) ARRAY(name_OF_SIZE_size) 
+
+#define ___FUNCTION(name, args, type) ___DECLARE(name args, type)
 #define FUNCTION(name_OF_args_AS_type) ___FUNCTION(name_OF_args_AS_type)
 #define STARTFUNCTION {
 #define ENDFUNCTION }
@@ -33,19 +41,8 @@
 #define ___FUNCTION_POINTER(name, args) (*name) args
 #define FUNCTION_POINTER(name_OF_args) ___FUNCTION_POINTER(name_OF_args)
 
-// FUNCTION( FUNCTION_POINTER( ff( DECLARE(x AS double), DECLARE(y AS float)) OF (DECLARE(n AS int) OF DECLARE(k AS short))) AS float );
-// DECLARE_AS_FUNCTION(f(double x, float y) AS ((int n, short k) AS float));
-// g(char) -> ( (short) -> ( (int) -> long ) )
-// long (*)(int i);
-// (*g(char c))(short h)
-// DEC_AS_FUN(g(char c) AS ((short h) AS))
-// DECLARE_AS_FUNCTION(DECLARE_AS_FUNCTION( g(DECLARE(c AS char)) AS ((DECLARE(h AS short)) AS)) AS ((DECLARE(i AS int)) AS long));
-// FUNCTION(FUNCTION_POINTER(FUNCTION_POINTER(G(char) OF (short)) OF (int)) AS long);
-
-
-
 #define STRUCT(name) typedef struct name name; struct name {
 #define ENDSTRUCT };
 
-// float (*(*f( double x, float y)) (int n, short k))(void* p);
-// FUNCTION( FUNCTION_POINTER( FUNCTION_POINTER( f( DECLARE(x AS double), DECLARE(y AS float) ) OF (DECLARE(n AS int), DECLARE(k AS short)) ) OF (DECLARE(p AS void*)) ) AS float)
+#define RETURN return
+#define EXTERNAL extern
